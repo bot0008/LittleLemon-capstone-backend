93b849ce09fe50a930206b4c67a8d70ed20312a7
bot0008 <anemailbot@gmail.com>
2023-08-09 13:41:39 -0700

    booking, menu models

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
 # Register your models here.
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'inventory')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'no_of_guests', 'booking_date')    
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveBigIntegerField()
+    
+    def __str__(self):
+        return self.title
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.PositiveBigIntegerField()
+    booking_date = models.DateTimeField()
+    
+    def __str__(self):
+        return f"Booking by {self.customer_name} - {self.booking_date}"     
