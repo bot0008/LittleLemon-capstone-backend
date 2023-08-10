4a30db689818c057722bc3cc5edee0a09585c225
bot0008 <anemailbot@gmail.com>
2023-08-10 09:14:37 -0700

    Booking

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingViewSet
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
     path('index.html', TemplateView.as_view(template_name='index.html'), name='index'),
     path('restaurant/menu/',include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from rest_framework import generics
 from .models import Menu
 from .serializers import MenuSerializer
+from .models import Booking
+from .serializers import BookingSerializer
+from rest_framework import viewsets, permissions
 
 # Create your views here.
 def sayHello(request):
@@ -19,5 +22,12 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+        
+    
     
         
